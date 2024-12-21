91085bef39e2deab2f4ce23f8f8231bb6ee37992
Iyed Souilem <iyedsouilem2004@gmail.com>
2024-12-21 15:04:24 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,93 +1,89 @@
 #include <stdio.h>
 
+// Function to display a menu and get the user's choice
+int custom_menu(const char*title,const char*options[],int num_op)
+{
+    int choice;
+    printf("%s\n",title);
+    for (int i = 0; i < num_op; i++){
+        printf("%d. %s\n", i + 1, options[i]);
+    }
+    printf("Please select a number between 1 and %d: ",num_op);
+    scanf("%d", &choice);
+    while (!(choice>=1 && choice<=num_op)){
+        printf("Invalid choice, please try again (must be between 1 and %d): ", num_op);
+        scanf("%d",&choice);
+    }
+    return choice;
+}
 
-//main menu
-int main(){
+// Main menu
+int main()
+{
     int choice1, choice2;
+    const char*main_menu_options[]={
+        "Hotel Administrator",
+        "Employee",
+        "Customer",
+        "Exit"
+    };
+    const char*sub_menu_options[]={
+        "Register",
+        "Login",
+        "Back to main menu"
+    };
+
     while(1){
-        printf("\t\t\t**Welcome to the Hotel Management System!\n");
-        printf("\t~~\n");
-        printf("Please select your role:\n");
-        printf("1. Hotel Administrator\n");
-        printf("2. Employee\n");
-        printf("3. Customer\n");
-        printf("4. Exit\n");
-        printf("Please select a number between 1 and 4: ");
-        scanf("%d", &choice1);
-        while(!(choice1>=1 && choice1<=4)){
-            printf("Invalid choice, please try again(must be between 1 and 4): ");
-            scanf("%d",&choice1);
-        }
-         switch (choice1) {
-                case 1:
-                printf("You selected: Hotel Administrator\n");
-                printf("1. Register\n");
-                printf("2. Login\n");
-                printf("3. Back to main menu\n");
-                printf("Please enter your choice: :");
-                scanf("%d", &choice2);
-                while(!(choice2>=1 && choice2<=3)){
-                printf("Invalid choice, please try again(must be between 1 and 3): ");
-                scanf("%d",&choice2);}
-                switch (choice2)
+        choice1=custom_menu("\t\t\t**Welcome to the Hotel Management System!\n\t~~\nPlease select your role:",main_menu_options,4);
+        switch(choice1){
+            case 1:
+                printf("\t#### You selected: Hotel Administrator ####\t\n");
+                choice2 = custom_menu("Please enter your choice:",sub_menu_options,3);
+                switch(choice2)
                 {
                     case 1:
-                    //register_Hotel_Administrator();
-                    break;
+                        // register_Hotel_Administrator();
+                        break;
                     case 2:
-                    //login_Hotel_Administrator();
-                    break;
+                        // login_Hotel_Administrator();
+                        break;
                     case 3:
-                    break;
+                        break;
                 }
                 break;
-                case 2:
-                printf("You selected: Employee\n");
-                printf("1. Register\n");
-                printf("2. Login\n");
-                printf("3. Back to main menu\n");
-                printf("Please enter your choice: :");
-                scanf("%d", &choice2);
-                while(!(choice2>=1 && choice2<=3)){
-                printf("Invalid choice, please try again(must be between 1 and 3): ");
-                scanf("%d",&choice2);}
-                switch (choice2)
+            case 2:
+                printf("\t#### You selected: Employee ####\t\n");
+                choice2 = custom_menu("Please enter your choice:",sub_menu_options,3);
+                switch(choice2)
                 {
                     case 1:
-                    //register_employee();
-                    break;
+                        // register_employee();
+                        break;
                     case 2:
-                    //login_employee();
-                    break;
+                        // login_employee();
+                        break;
                     case 3:
-                    break;
+                        break;
                 }
-               break;
-               case 3:
-               printf("You selected: Customer\n");
-                printf("1. Register\n");
-                printf("2. Login\n");
-                printf("3. Back to main menu\n");
-                printf("Please enter your choice: :");
-                scanf("%d", &choice2);
-                while(!(choice2>=1 && choice2<=3)){
-                printf("Invalid choice, please try again(must be between 1 and 3): ");
-                scanf("%d",&choice2);}
-                switch (choice2)
+                break;
+            case 3:
+                printf("\t#### You selected: Customer ####\t\n");
+                choice2=custom_menu("Please enter your choice:",sub_menu_options,3);
+                switch(choice2)
                 {
                     case 1:
-                    //register_costumer();
-                    break;
+                        // register_customer();
+                        break;
                     case 2:
-                    //login_costumer();
-                    break;
+                        // login_customer();
+                        break;
                     case 3:
-                    break;
+                        break;
                 }
-               break;
-               case 4:
-               printf("Exiting the system. Goodbye!\n");
-               return 0;
+                break;
+            case 4:
+                printf("Exiting the system. Goodbye!\n");
+                return 0;
         }
     }
 }
